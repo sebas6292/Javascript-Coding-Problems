8fa767433f70ecb88197f73cda5626f1b3cf6e00
sebas6292 <64964750+sebas6292@users.noreply.github.com>
2022-10-15 11:23:58 -0700

    work on bug

--- index.js
+++ index.js
@@ -218,13 +218,22 @@ const largestPair = (array) => {
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-const twoSumPair = (nums, target) => {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; i++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-};
-console.log(twoSumPair([1, 2, 3, 4, 5, 6], 12));
+// const twoSumPair = (nums, target) => {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; i++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; i++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+// console.log(twoSum([1, 2, 3, 4, 5, 6], 12));
