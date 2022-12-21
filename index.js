d24fce12a01a05369a1bb412712450417b16ac75
sebas6292 <64964750+sebas6292@users.noreply.github.com>
2022-12-20 20:12:50 -0800

    1 file modified

--- index.js
+++ index.js
@@ -311,3 +311,13 @@ const occurences = array.reduce((acc, curr) => {
 }, {});
 
 // console.log(occurences);
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 15 === 0) {
+    console.log(i, 'fizzbuzz');
+  } else if (i % 5 === 0) {
+    console.log(i, 'buzz');
+  } else if (i % 3 === 0) {
+    console.log(i, 'fizz');
+  }
+}
